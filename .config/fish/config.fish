d6d7c2455a0d247606d1703bf72a4340b2608ff9
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2025-10-15 10:48:02 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,17 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here
 
-  # tmux
-  set -gx TMUX_TMPDIR /tmp
+    # tmux
+    set -gx TMUX_TMPDIR /tmp
 
-  # bun
-  set --export BUN_INSTALL "$HOME/.bun"
-  set --export PATH $BUN_INSTALL/bin $PATH
+    # bun
+    set --export BUN_INSTALL "$HOME/.bun"
+    set --export PATH $BUN_INSTALL/bin $PATH
 
-  # VS Code
-  set --export PATH /mnt/c/Users/Jakob.Wegenschimmel/AppData/Local/Programs/Microsoft\ VS\ Code/bin $PATH
+    # VS Code
+    set --export PATH /mnt/c/Users/Jakob.Wegenschimmel/AppData/Local/Programs/Microsoft\ VS\ Code/bin $PATH
 
-  tmux
+    zoxide init fish | source
+
+    tmux attach
 end
